b46fda6b258c2dd4557006372f68e38729b3b0bd
AndreiAta <andreww0007@gmail.com>
2023-04-21 12:13:27 +0200

    Add project files.

---
+++ BlazorCompanyEventsApp/Shared/Participant.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCompanyEventsApp.Shared;
+
+[Serializable]
+public class Participant
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string PhoneNumber { get; set; }
+}
