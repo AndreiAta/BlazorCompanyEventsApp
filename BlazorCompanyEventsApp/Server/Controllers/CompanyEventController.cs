57190af2e8c5d5aa2b6ce418e549d8f4835f04e4
AndreiAta <andreww0007@gmail.com>
2023-04-21 12:42:46 +0200

    Add Company Event class and controller as well as Razor components

---
+++ BlazorCompanyEventsApp/Server/Controllers/CompanyEventController.cs
@@ -0,0 +1,15 @@
+﻿using BlazorCompanyEventsApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorCompanyEventsApp.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CompanyEventController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IReadOnlyCollection<CompanyEvent>> GetAll()
+    {
+
+    }
+}
