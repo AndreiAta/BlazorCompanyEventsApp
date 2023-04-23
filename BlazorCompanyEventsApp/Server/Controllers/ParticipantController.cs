35476b360af6bdffd8f277b6ad470c6ad5e7425a
AndreiAta <andreww0007@gmail.com>
2023-04-23 15:34:46 +0200

    Create participant controller

---
+++ BlazorCompanyEventsApp/Server/Controllers/ParticipantController.cs
@@ -0,0 +1,35 @@
+﻿using BlazorCompanyEventsApp.Server.Data;
+using BlazorCompanyEventsApp.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorCompanyEventsApp.Server.Controllers;
+
+public class ParticipantController : ControllerBase
+{
+    private readonly ILogger<CompanyEventController> _logger;
+    private readonly IParticipantRepository _participantRepository;
+
+    public ParticipantController(IParticipantRepository participantRepository, ILogger<CompanyEventController> logger)
+    {
+        _participantRepository = participantRepository;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IReadOnlyCollection<Participant>> GetParticipantsForEvent(int companyEventId)
+    {
+        return await _participantRepository.GetParticipantsByCompanyEventId(companyEventId);
+    }
+
+    [HttpPost]
+    public async Task CreateParticipant(Participant participant)
+    {
+        await _participantRepository.CreateParticipant(participant);
+    }
+
+    [HttpPut]
+    public async Task UpdateParticipant(Participant participant)
+    {
+        await _participantRepository.UpdateParticipant(participant);
+    }
+}
